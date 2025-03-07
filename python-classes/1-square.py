a4d88c33010171e7e2712acdedd107634f3599fd
Jonathan2055 <j.mugisha3@alustudent.com>
2025-03-07 11:17:29 -0800

    Debugging

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,7 @@ class Square:
     """Defines a square."""
     
 
-def __init__(self, size):
+    def __init__(self, size):
         """Initialize a square with a given size."""
         self.__size = size
 
