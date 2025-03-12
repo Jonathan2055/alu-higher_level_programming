8437e72a2e4dd067c30ff6e2edfa5cbab7ba1b66
Jonathan2055 <j.mugisha3@alustudent.com>
2025-03-12 05:29:07 -0700

    Debug

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,5 @@
 """Write a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
 
 
-def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as file:
+with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
