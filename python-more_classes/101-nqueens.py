c1b9124a85a4ef0966b35ddd99ed7ac72e96d74e
Jonathan MUGISHA <j.mugisha3@alustudent.com>
2025-03-15 00:11:20 +0200

    Update 101-nqueens.py

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Solves the N-Queens problem by finding all valid arrangements of N queens on an N×N 
-chessboard without any two queens attacking each other.
-
-Example Usage:
-ruby
-Copy code
-$ ./101-nqueens.py N  
-Where N is an integer value of 4 or greater.
-
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+Example:
+    $ ./101-nqueens.py N
+N must be an integer greater than or equal to 4.
 Attributes:
-board (list): A nested list representing the chessboard.
-solutions (list): A collection of valid queen placements.
-Each solution is structured as [[r, c], [r, c], [r, c], [r, c]], where r 
-denotes the row and c denotes the column, indicating the positions of the queens on the board.
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
 """
-
-
 import sys
 
 
