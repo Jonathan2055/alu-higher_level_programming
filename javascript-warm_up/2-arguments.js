f1ee1bc0337dd62f1cfb1387cc24cb85298c2f2d
Jonathan MUGISHA <j.mugisha3@alustudent.com>
2025-05-31 22:47:11 +0200

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const count = process.argv.length - 2;
-if (count === 0) {
-	  console.log('No argument');
-} else if (count === 1) {
-	  console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }
